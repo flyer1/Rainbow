1b03ab8a3bef0d4a4357fa52e62cf938ac6d9855
Jon Kroeker <jon.kroeker2@gmail.com>
2015-10-24 10:55:36 -0500

    Nav cleanup

--- src/client/js/layout/shell.controller.js
+++ src/client/js/layout/shell.controller.js
@@ -5,20 +5,27 @@
         .module('app')
         .controller('ShellController', ShellController);
 
-    ShellController.$inject = ['datacontext'];
+    ShellController.$inject = ['$location', 'datacontext'];
 
-    function ShellController(datacontext) {
+    function ShellController($location, datacontext) {
         var vm = this;
 
         var siteRepo = datacontext.getSiteRepository();
         vm.sites = siteRepo.sites;
-        vm.currNavItem = 'home';
+        vm.currNavItem = '';
         vm.setActive = setActive;
         vm.isActive = isActive;
 
+        init();
         return;
 
         /////////// IMPLEMENTATION /////////////////
+        function init() {
+            var urlAry = $location.url().split('/');
+            var tailUrl = urlAry[urlAry.length - 1];
+            vm.currNavItem = tailUrl;
+        }
+
         function setActive(navItem) {
           vm.currNavItem = navItem.code;
           console.log('active nav is ' + vm.currNavItem );
