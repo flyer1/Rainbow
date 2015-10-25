eef952adcb33aa20da181a4e25a6112510644b7a
Jon Kroeker <jon.kroeker2@gmail.com>
2015-10-25 17:24:06 -0500

    Restructured the announcement and added it to P5 site details.

--- src/client/js/data/site.data.js
+++ src/client/js/data/site.data.js
@@ -408,6 +408,9 @@
                     },
                 ],
                 introduction: 'Expected opening date is January 2017',
+                message: ['Rainbow is very excited to announce that we were the successful candidates in the RFP for the new Daycare opening at École Sage Creek. This daycare is projected to open in January of 2017 and will have 16 infant spots, 8 early years (age 2), 32 preschool (age 3/4), 10 Kindergarten (age 4/5) and 30 school age spots.',
+                          'Rainbow will be using the Manitoba Government Online Child Care Registry to fill these spots. The projected date for signing up online is Summer of 2016. Rainbow will not be taking a list until that time. Parents who need Daycare prior to this are encouraged to register online at one of our other Rainbow locations.',
+                          'Rainbow is committed to keeping the Community and our Families updated on our progress as we prepare for this exciting new opportunity.'],
                 order: 7,
             };
 
--- src/client/js/directives/controls/rdc-banner-message.js
+++ src/client/js/directives/controls/rdc-banner-message.js
@@ -3,35 +3,17 @@
 
     angular
         .module('app.directives')
-        .directive('rdnBannerMessage', rdnBannerMessage);
+        .directive('rdcBannerMessage', rdcBannerMessage);
 
-        rdnBannerMessage.$inject = ['popoverMessageService'];
-
-    function rdnBannerMessage(popoverMessageService) {
+    function rdcBannerMessage() {
         var directive = {
-            link: link,
             scope: {
-                'message': '=',
+                'data': '=',
             },
             templateUrl: '/js/directives/controls/rdc-banner-message.html',
             restrict: 'E'
         };
         return directive;
 
-        /////////////////// IMPLEMENTATION ///////////////////
-        function link(scope, element, attrs) {
-            scope.showDetails = showDetails;
-
-            return;
-            ///////////
-
-            function showDetails() {
-                var options = {
-                        targetId: 'banner-message-toggle',
-                        content: scope.message.full
-                };
-                popoverMessageService.show(options);
-            }
-        }
     }
 })();
