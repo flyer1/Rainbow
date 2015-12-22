38f313a66673e34c07f9991a093ebe59c49c8709
Jon Kroeker <jon.kroeker2@gmail.com>
2015-12-22 15:46:07 -0600

    Fixed banner changes.

--- src/client/js/pages/site-details/site-details.controller.js
+++ src/client/js/pages/site-details/site-details.controller.js
@@ -9,11 +9,17 @@
 
     function SiteDetailsController($document, $window, $stateParams, siteService, siteData) {
         var vm = this;
+
+        // Properties
         vm.site = {};
         vm.photos = [];
         vm.prevSite = {};
         vm.nextSite = {};
         vm.mapWidth = 0;
+
+        // Functions
+        vm.toTop = toTop;
+
         var code = $stateParams.id;
 
         init(siteData);
@@ -32,13 +38,13 @@
 
             vm.photos = siteService.getSitePhotos(vm.site, vm.site.photos);
 
-            $document.scrollTo(top);
+            toTop();
         }
 
         function initBanner() {
             var lastAction = '';
             var fixedBanner = angular.element('.site-banner-fixed');
-            angular.element($window).bind('scroll', _.throttle(onScroll, 100));
+            angular.element($window).bind('scroll', _.throttle(onScroll, 100)); // TODO: unbind on destroy of the controller
 
             function onScroll() {
                 var triggerY = 350;
@@ -56,54 +62,6 @@
             }
         }
 
-        // TODO: fix up this mess...
-        // Also when do you turn off the event listener? 
-        function initJon() {
-            var header = $('.site-banner');
-            var headerHeight = header.height();
-            var title = header.find('.section .container h1');
-            var y = title.position().top;
-            var titleHeight = title.height();
-            var lastAction = '';
-            var action = '';
-
-            function stickyScroll(e) {
-                var className = '';
-
-                if ($window.innerWidth < 767) return;
-
-                if ($window.pageYOffset > y) {
-                    action = 'addClass';
-                    className = 'fixed';
-                }
-
-                if ($window.pageYOffset < y) {
-                    action = 'removeClass';
-                    className = 'fixed';
-
-                }
-
-                if ($window.pageYOffset > 270) {
-                    header.addClass('fixed-2');
-                }
-
-                if ($window.pageYOffset < 270) {
-                    header.removeClass('fixed-2');
-                }
-
-                if (action !== lastAction) {
-                    console.log('performing action')
-                    header[action](className);
-                    lastAction = action;
-                }
-
-            }
-
-            // Scroll handler to toggle classes.
-            console.log('binding to scroll...') // TODO: Yeah, gotta unblind this sucka as I'm hooking up additional listeners with each nav to site details..
-            angular.element($window).bind('scroll', _.throttle(stickyScroll, 50));
-        }
-
         // Init the data that drives the prev/next buttons 
         function initSiteNav() {
             var index = _.indexOf(siteData.sites, vm.site);
@@ -113,6 +71,15 @@
 
             vm.prevSite = siteData.sites[prevIndex];
             vm.nextSite = siteData.sites[nextIndex];
+        }
+
+        function toTop(animated) {
+
+            if (animated) {
+                $document.scrollTopAnimated(0, 1000);
+            } else {
+                $document.scrollTo(top);
+            }
         }
 
     }
