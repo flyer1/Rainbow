729b2cb07fd5cb62fedf92f81b0c97917a96436d
Jon Kroeker <jon.kroeker2@gmail.com>
2017-09-06 22:14:16 -0500

    Updates for sage creek daycare phase 5 now that its open

--- src/client/js/services/web-data.service.js
+++ src/client/js/services/web-data.service.js
@@ -22,38 +22,38 @@
         return service;
 
         //////////////////////// IMPLEMENTATION /////////////////////
+        // WARNING!!! Because I don't have cache busting in place for json files - the v paramater below in all of the service calls has to be bumped up whenever the data changes. Certainly not a great strategy but it's what we have right now.
         function getSites() {
-            return get('/data/site.data.json');
+            return get('/data/site.data.json?v1.1');
         }
         function getSchools() {
-            return get('/data/school.data.json');
+            return get('/data/school.data.json?v1.1');
         }
 
         function getPrograms() {
-            return get('/data/program.data.json');
+            return get('/data/program.data.json?v1.1');
         }
 
         function getRefTransport() {
-            return get('/data/ref-transport.data.json');
+            return get('/data/ref-transport.data.json?v1.1');
         }
 
         function getMessages() {
-            return get('/data/message.data.json');
+            return get('/data/message.data.json?v1.1');
         }
 
         function getCoverPhotos() {
-            return get('/data/cover-photo.data.json');
+            return get('/data/cover-photo.data.json?v1.1');
         }
 
         function getChangeLog() {
-            return get('/data/change-log.data.json');
+            return get('/data/change-log.data.json?v1.1');
         }
 
         function get(url) {
             var deferred = $q.defer();
 
-            $http.get(url)
-                .then(function (response) {
+            $http.get(url).then(function (response) {
                     deferred.resolve(response.data);
                 })
                 .catch(function (response) {
